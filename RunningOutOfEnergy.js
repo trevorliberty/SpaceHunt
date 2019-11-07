4013008d640b762312cd64df7423d47ba20f352f
micahblasko <mblasko@pdx.edu>
2019-11-07 15:39:59 -0800

    Update RunningOutOfEnergy.js

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -1,6 +1,6 @@
 function checkEnergy() {
    if(document.UI.energy.value <= 0){
-       alert("You died!!");
+       alert("You silly! You ran out of gosh darn energy! You will not be resurrected.");
        //if game config is set to reular play
        //   game ends() 
    }
