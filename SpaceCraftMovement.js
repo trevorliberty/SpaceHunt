198c111808d2a55426fa731115c9086daf9d7962
trevorliberty <tliberty@pdx.edu>
2019-11-12 11:44:05 -0800

    changed spacecraftmovement.js. coordinates are now updated in input & localstorage

--- SpaceCraftMovement.js
+++ SpaceCraftMovement.js
@@ -1,9 +1,46 @@
-function moveSpacecraft(angle,distance) {
-   alert("Move Space Craft");
-   checkEnergy();
-   checkSupplies();
-   //0 is east
-    //90 north
-    //180 west
-    //270 south
+function moveSpacecraft(angle, distance) {
+  let xCoordloc = parseInt(document.getElementById("xCoord").value);
+  let yCoordloc = parseInt(document.getElementById("yCoord").value);
+  distance = parseInt(distance);
+  if (angle === 0) {
+    if (parseInt(xCoordloc - distance < 0)) {
+      //fallen off the world
+      console.log("out of bounds");
+    } else {
+      xCoordloc -= distance;
+      document.getElementById("xCoord").value = xCoordloc;
+    }
+  } else if (angle === 90) {
+    if (parseInt(yCoordloc + distance) > 128) {
+      //fallen off the world
+      console.log("out of bounds");
+    } else {
+      yCoordloc += distance;
+    }
+  } else if (angle === 180) {
+    if (xCoordloc - distance > 128) {
+      //fallen off the world
+      console.log("out of bounds");
+    } else {
+      xCoordloc += distance;
+    }
+  } else {
+    //angle is 270
+    if (yCoordloc - distance < 0) {
+      //fallen off the world
+      console.log("out of bounds");
+    } else {
+      yCoordloc -= distance;
+    }
+  }
+
+  document.getElementById("xCoord").value = xCoordloc;
+  document.getElementById("yCoord").value = yCoordloc;
+
+  localStorage.setItem("xCoord", toString(xCoordloc));
+  localStorage.setItem("yCoord", toString(yCoordloc));
+  //0 is east
+  //90 north
+  //180 west
+  //270 south
 }
--- trevorliberty.js
+++ trevorliberty.js
@@ -1,18 +1,18 @@
-function makeboard(rows, cols){
-    const container = document.getElementById("container");
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell).className = "grid-item";
-    };
-};
-function load(){
-    localStorage.setItem('health', '100');
-    localStorage.setItem('supplies', '100');
-    localStorage.setItem('credits', '100');
-    localStorage.setItem('xCoordinate', '0'); 
-    localStorage.setItem('yCoordinate', '0');
-    makeboard(5,5);
+function makeboard(rows, cols) {
+  const container = document.getElementById("container");
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+function load() {
+  localStorage.setItem("health", "100");
+  localStorage.setItem("supplies", "100");
+  localStorage.setItem("credits", "100");
+  localStorage.setItem("xCoord", "0");
+  localStorage.setItem("yCoord", "0");
+  makeboard(5, 5);
 }
