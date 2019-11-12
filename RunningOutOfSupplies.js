85b8815a8353beb7113d51d7e8bf8fb477161e3f
Bo-Zhang-819 <56896198+Bo-Zhang-819@users.noreply.github.com>
2019-11-12 15:54:10 -0800

    Update RunningOutOfSupplies.js

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -1,7 +1,7 @@
 function checkSupplies() {
-
-		if(document.UI.supplies.value <= 1){
-            alert("Attention! Your Supplies almost done!");
+    var gameType = document.UI.gameType.value;
+		if(gameType == 0){
+            //alert("Attention! Your Supplies almost done!");
             if(document.UI.supplies.value == 0)
             {
                 alert("Your Supplies is done! Game Over!");
