1900fe053879084c30ccb88ae18e88f3a96ce08b
Micah Blasko <mblasko@pdx.edu>
2019-11-12 15:52:31 -0800

    Update RunningOutOfEnergy.js

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -2,7 +2,7 @@ function checkEnergy() {
     var gameType = document.UI.gameType.value;
     if(gameType == 0){
         if(document.UI.energy.value <= 0){
-            alert("You silly! You ran out of gosh darn energy! You will not be  resurrected.");
+            alert("You silly! You ran out of energy! You will not be  resurrected.");
             //game ends
         }
     }
