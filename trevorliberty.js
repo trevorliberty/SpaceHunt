26223f54d1bccca93e88d28f9fe42d5031900fc9
trevorliberty <tliberty@pdx.edu>
2019-11-13 14:23:12 -0800

    spaceship flies around map

--- trevorliberty.js
+++ trevorliberty.js
@@ -1,9 +1,56 @@
+window.addEventListener("keydown", function(e) {
+  if (
+    (e.keyCode == 38 ||
+      e.keyCode == 39 ||
+      e.keyCode == 37 ||
+      e.keyCode == 40) &&
+    e.target == document.body
+  ) {
+    e.preventDefault();
+  }
+});
+saveCell = {
+  xCoord: 0,
+  yCoord: 0,
+  id: "0,0"
+};
+document.onkeydown = function(e) {
+  if (
+    e.keyCode === 39 ||
+    e.keyCode === 38 ||
+    e.keyCode === 37 ||
+    e.keyCode === 40
+  ) {
+    if (e.keyCode == 39) {
+      //right
+      moveSpacecraft(90, 1);
+      return;
+      9;
+    } else if (e.keyCode == 37) {
+      moveSpacecraft(270, 1);
+      return;
+      //left
+    } else if (e.keyCode == 38) {
+      //up
+      moveSpacecraft(0, 1);
+      return;
+    } else if (e.keyCode == 40) {
+      moveSpacecraft(180, 1);
+      return;
+      //down
+    }
+    document
+      .getElementById("container")
+      .scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+};
+
 function makeboard(rows, cols) {
   const container = document.getElementById("container");
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows; c++) {
-    for (j = 0; j < cols; j++) {
+  for (c = rows; c >= 0; c--) {
+    for (j = 0; j < cols; ++j) {
       let cell = document.createElement("div");
       cell.setAttribute("id", `${c + "," + j}`);
       container.appendChild(cell).className = "grid-item";
@@ -11,16 +58,45 @@ function makeboard(rows, cols) {
       space.setAttribute("id", "space");
       space.style.width = "20px";
       space.style.height = "20px";
-      /*let img = document.createElement("img");
-      img.style.width = "100%";
-      img.style.height = "auto";
-      img.setAttribute("src", "./ship.png");*/
       cell.appendChild(space);
-      //space.appendChild(img);
     }
   }
 }
+
+function retrieveCell() {
+  const xCoord = parseInt(document.getElementById("xCoord").value);
+  const yCoord = parseInt(document.getElementById("yCoord").value);
+  const x = document.getElementById("xCoord").value;
+  const y = document.getElementById("yCoord").value;
+  const id = saveCell.id;
+  let obj = {
+    x: xCoord,
+    y: yCoord,
+    id: id
+  };
+  return obj;
+}
+
+function updateCell(x, y, lastCell) {
+  saveCell.id = lastCell.id;
+  document.getElementById("xCoord").value = x;
+  document.getElementById("yCoord").value = y;
+  saveNode = document.getElementById("ship");
+  if (saveNode !== null) {
+    saveNode.id = `${lastCell.x + "," + lastCell.y}`;
+  }
+  let elem = getElement(x, y);
+  elem.setAttribute("id", "ship");
+  localStorage.setItem("xCoord", toString(x));
+  localStorage.setItem("yCoord", toString(y));
+}
+
+function getElement(x, y) {
+  return document.getElementById(`${x + "," + y}`);
+}
+
 function load() {
+  //check first if there is a value at these localstorage items
   localStorage.setItem("energy", "1000");
   localStorage.setItem("supplies", "100");
   localStorage.setItem("credits", "100");
@@ -33,4 +109,18 @@ function load() {
   document.getElementById("energy").value = 1000;
   document.getElementById("supplies").value = 100;
   makeboard(25, 25);
+  saveCell.id = "0,0";
+  shipInit();
 }
+
+function shipInit() {
+  currentCell = retrieveCell();
+  let x = currentCell.x;
+  let y = currentCell.y;
+  updateCell(x, y, currentCell);
+}
+
+let ship = {
+  xCoord: 0,
+  yCoord: 0
+};
