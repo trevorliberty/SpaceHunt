a8f11127d1805af7daa810f7ee5272494d6ab039
tjnuccio <tj_nuccio@yahoo.com>
2019-11-12 22:17:50 -0800

    Updated control flow for files. Starts at index for selecting user type, then goes to either settings or map page. Still figuring out how to populate map values if administrator is used.

---
+++ configuration.js
@@ -0,0 +1,34 @@
+var configuration = {
+    energy : "1000",
+    supplies : "100",
+    credits : "100",
+    xCoord : "0",
+    yCoord : "0",
+    dies : true,
+    wormholeFixed : true,
+};
+
+function configure() {
+
+    // configuration.energy = parseInt(document.getElementById("energy").value);
+    // configuration.supplies = parseInt(document.getElementById("supplies").value);
+    // configuration.credits = parseInt(document.getElementById("credits").value);
+    // configuration.xCoord = parseInt(document.getElementById("x").value);
+    // configuration.yCoord = parseInt(document.getElementById("y").value);
+    //
+    // if(document.getElementById('dying').checked) {
+    //     configuration.dies = JSON.parse(document.getElementById('dying').value);
+    // }
+    // else if (document.getElementById('godMode').checked) {
+    //     configuration.dies = JSON.parse(document.getElementById('godMode').value);
+    // }
+    //
+    // if(document.getElementById('fixedWormhole').checked) {
+    //     configuration.dies = JSON.parse(document.getElementById('fixedWormhole').value);
+    // }
+    // else if (document.getElementById('randomWormhole')) {
+    //     configuration.dies = JSON.parse(document.getElementById('randomWormhole').value);
+    // }
+    window.location.href = "SpaceHunt Core HTML.html";
+
+}
