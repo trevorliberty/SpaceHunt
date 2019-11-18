b80e1b6f2ad16a45e8e33d6ffb14434a5b525466
Bo-Zhang-819 <56896198+Bo-Zhang-819@users.noreply.github.com>
2019-11-18 12:32:48 -0800

    Update RunningOutOfEnergy.js

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -1,8 +1,12 @@
 function checkEnergy() {
-    if(localStorage.getItem("dies")){
-        if(localStorage.getItem("energy") <= 0){
-            alert("You silly! You ran out of energy! You will not be  resurrected.");
-            //game ends
-        }
+  if (localStorage.getItem("dies")) {
+    if (
+      energy <= 0 ||
+      document.getElementById("energy").value <= 0 ||
+      localStorage.getItem("energy") <= 0
+    ) {
+      alert("You silly! You ran out of energy! You will not be  resurrected.");
+      //game ends
     }
+  }
 }
