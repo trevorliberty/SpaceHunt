5d39c70d4342202fc9413a06e4404de18aa87706
tjnuccio <tj_nuccio@yahoo.com>
2019-11-19 22:17:59 -0800

    Update RunningOutOfSupplies.js

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -1,35 +1,14 @@
 function checkSupplies() {
-//  var gameType = document.UI.gameType.value;
-//  if (gameType == 0) {
-    //regular game type
-    //var docSupplies = document.getElementById("supplies");
-      
-    /*if (supplies == 2 ||
-        docSupplies.value == 2 ||
-        localStorage.getItem("supplies") == 2) {
-        
-      alert("Attention! Your Supplies almost done!");
-        
-    }
-    else if (supplies <= 0 ||
-             docSupplies.value <= 0 ||
-             localStorage.getItem("supplies") <= 0){
-        if(localStorage.getItem("dies")) {
-           alert("Your Supplies is done! Game Over!"); // game ends
-            window.location.href = "index.html";
-        }
-    }
-  }*/
-    //otherwise, nodie game type
-    if(localStorage.getItem("dies"))
-    {
-        if(supplies <= 0||
-           document.getElementById("supplies").value <= 0||
-           localStorage.getItem("supplies") <= 0){
-            alert("Your Supplies run out! Game is over!");
-            window.location.href = "index.html";
-        }
-    }
-    //alert("gg");
-  
+  check = JSON.parse(localStorage.getItem("dies"));
+  if (check) {      //If check is true, player dies and game ends, else continues
+      if (
+          supplies <= 0 ||
+          document.getElementById("supplies").value <= 0 ||
+          localStorage.getItem("supplies") <= 0
+      ) {
+          alert("You've run out of supplies! Game over.");
+          //game ends
+          window.location.href = "index.html";
+      }
+  }
 }
