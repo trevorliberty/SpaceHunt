638687f1cf068873f391fd4669f306b3dcace0ab
Micah Blasko <mblasko@pdx.edu>
2019-11-21 15:11:13 -0800

    Update micahblasko.js

--- micahblasko.js
+++ micahblasko.js
@@ -1,1 +1,21 @@
 //document.getElementById("energy") = localStorage.getItem("energy");
+function saveState(){
+    configure(document.getElementById("savestate-name").value)
+}
+
+function configure(name) {
+
+    var saveConfig = {};
+
+    saveConfig.energy = document.getElementById('energy').value;
+    saveConfig.supplies = document.getElementById('supplies').value;
+    saveConfig.credits = document.getElementById('credits').value;
+    saveConfig.xCoord = document.getElementById('xCoord').value;
+    saveConfig.yCoord = document.getElementById('yCoord').value;
+    saveConfig.maxSize = document.getElementById('maxSize').value;
+    saveConfig.wormholeFixed = document.getElementById('wormholeFixed').value;
+    saveConfig.dies = document.getElementById('dies').value;
+
+
+    localStorage.setItem(name, JSON.stringify(saveConfig));
+}
