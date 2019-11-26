8cf333933cefa437eb28cf64bdb12fc3a797f743
tjnuccio <tj_nuccio@yahoo.com>
2019-11-26 15:39:42 -0800

    Merge branch 'master' of https://github.com/trevorliberty/Team-5

--- micahblasko.js
+++ micahblasko.js
@@ -1,32 +1,25 @@
-
-
 //document.getElementById("energy") = localStorage.getItem("energy");
 function saveState(){
     configure(prompt("Enter Save Name:"));
 }
-
 function configure(name) {
-
     var saveConfig = {};
-
     saveConfig.energy = document.getElementById('energy').value;
     saveConfig.supplies = document.getElementById('supplies').value;
     saveConfig.credits = document.getElementById('credits').value;
     saveConfig.xCoord = document.getElementById('xCoord').value;
     saveConfig.yCoord = document.getElementById('yCoord').value;
-    saveConfig.maxSize = document.getElementById('maxSize').value;
-    saveConfig.wormholeFixed = document.getElementById('wormholeFixed').value;
-    saveConfig.dies = document.getElementById('dies').value;
-
-
+    saveConfig.maxSize = parseInt(localStorage.getItem('maxSize'));
+    saveConfig.wormholeFixed = localStorage.getItem('wormholeFixed');
+    saveConfig.dies = localStorage.getItem('dies');
     localStorage.setItem(name, JSON.stringify(saveConfig));
 }
-
 function loadSaveState() {
     name = prompt("Enter name to load:");
-    
+    localStorage.removeItem('config');
     configObj = JSON.parse(localStorage.getItem(name));
-  
+    localStorage.setItem('config', JSON.stringify(configObj));
+    window.location.href = "SpaceHunt Core HTML.html";
     localStorage.setItem("energy", configObj.energy);
     localStorage.setItem("supplies", configObj.supplies);
     localStorage.setItem("credits", configObj.credits);
@@ -37,33 +30,28 @@ function loadSaveState() {
     localStorage.setItem("dies", JSON.stringify(dies));
     var wormholeFixed = configObj.wormholeFixed;
     localStorage.setItem("wormholeFixed", JSON.stringify(wormholeFixed));
-  
     var maxSize = parseInt(localStorage.getItem("maxSize"));
-  
-    /*  
     document.getElementById("energy").value = parseInt(
-      localStorage.getItem("energy")
+        localStorage.getItem("energy")
     );
     document.getElementById("supplies").value = parseInt(
-      localStorage.getItem("supplies")
+        localStorage.getItem("supplies")
     );
     document.getElementById("xCoord").value = parseInt(
-      localStorage.getItem("xCoord")
+        localStorage.getItem("xCoord")
     );
     document.getElementById("yCoord").value = parseInt(
-      localStorage.getItem("yCoord")
+        localStorage.getItem("yCoord")
     );
-    */
-  
     size = maxSize;
     makeboard(maxSize, maxSize + 1);
-  
     saveCell.id = "0,0";
     energy = parseInt(localStorage.getItem("energy"));
     supplies = parseInt(localStorage.getItem("supplies"));
     //credit = parseInt(localStorage.getItem("credits"));
     shipInit();
     document
-      .getElementById("ship")
-      .scrollIntoView({ behavior: "instant", block: "center" });
-  }
+        .getElementById("ship")
+        .scrollIntoView({ behavior: "instant", block: "center" });
+    window.location.href = "SpaceHunt Core HTML.html";
+}
